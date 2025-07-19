eebffeb16d648dd8ed07a3aac2712714ea4d9e4f
Lucas Lira <lucasliradev@gmail.com>
2025-07-19 11:42:43 -0300

    refatoração e organização de validação

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,14 +1,32 @@
 const { contas } = require("../database/database")
 
+// Verifica se existe alguma conta com o CPF informado
 const findAccountByCpf = (cpf) => {
-    return contas.find(account => account.usuario.cpf === cpf);
+    return contas.find(account => account.usuario.cpf === cpf)
 }
 
+// Verifica se existe alguma conta com o e-mail informado
 const findAccountByEmail = (email) => {
-    return contas.find(account => account.usuario.email === email);
+    return contas.find(account => account.usuario.email === email)
+}
+
+// Verifica se o CPF já está em uso por outra conta
+const isCpfInUse = (cpf, currentAccountNumber) => {
+    return contas.find(account => 
+        account.usuario.cpf === cpf && account.numero_conta !== currentAccountNumber
+    )
+}
+
+// Verifica se o e-mail já está em uso por outra conta
+const isEmailInUse = (email, currentAccountNumber) => {
+    return contas.find(account => 
+        account.usuario.email === email && account.numero_conta !== currentAccountNumber
+    )
 }
 
 module.exports = {
     findAccountByCpf,
     findAccountByEmail,
+    isCpfInUse,
+    isEmailInUse
 }
