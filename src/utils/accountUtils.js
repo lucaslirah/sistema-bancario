fba8141ffcb5f0ae97132e1a54a14bad659e5b14
lucaslirah <lucasliradev@gmail.com>
2025-07-22 23:26:55 -0300

    refatoração para uso de banco de dados sqlite - concluindo

--- src/utils/accountUtils.js
+++ src/utils/accountUtils.js
@@ -1,18 +1,22 @@
-const { contas } = require("../database/database")
+// const { contas } = require("../database/database")
+const connection = require("../database/connection")
 
 // Função para gerar número da conta a partir do telefone
-const generateAccountNumber = (telefone) => {
-  const AccountNumber = Number(telefone)
+const generateAccountNumber = async (telefone) => {
+  const AccountNumber = telefone
 
   // Validação: número precisa ter 11 dígitos numéricos
   if (!/^\d{11}$/.test(telefone)) {
     throw new Error("Número de telefone inválido para gerar conta.")
   }
 
-  // Verifica se já existe esse número como conta
-  const ExistingAccount = contas.find(
-    (account) => account.numero_conta === AccountNumber
-  )
+  // Verifica se já existe esse número como conta em connection
+  const ExistingAccount = await connection('contas')
+  .where({ numero_conta: AccountNumber })
+  .first()
+  // contas.find(
+  //   (account) => account.numero_conta === AccountNumber
+  // )
   if (ExistingAccount) {
     throw new Error("Este número de telefone já está associado a uma conta.")
   }
